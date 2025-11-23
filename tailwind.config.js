82cafb2cdf7d87c82b2cfddb78d994f6c476d83c
MelroseSaint <monroedoses@gmail.com>
2025-11-23 08:43:25 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'neon-cyan': '#00FFCC',
+                'neon-blue': '#1E90FF',
+                'neon-magenta': '#FF0080',
+            },
+            fontFamily: {
+                sans: ['Inter', 'system-ui', 'sans-serif'],
+                mono: ['JetBrains Mono', 'monospace'],
+            },
+        },
+    },
+    plugins: [],
+}
